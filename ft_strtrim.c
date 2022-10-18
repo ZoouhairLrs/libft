806e6f2afa57cdd57f994d008cc4af9deb6e519d
Zouhair Laaroussi <92701395+ZoouhairLrs@users.noreply.github.com>
2022-10-18 16:38:36 +0100

    last commit

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,13 +6,77 @@
 /*   By: zlaarous <zlaarous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 13:53:42 by zlaarous          #+#    #+#             */
-/*   Updated: 2022/10/18 13:57:09 by zlaarous         ###   ########.fr       */
+/*   Updated: 2022/10/18 16:24:34 by zlaarous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	isset(char c, const char *set)
+{
+	int	i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (set[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+static int	ftrimlen(int len, const char *s, const char *set)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = len - 1;
+	while (s[i] && isset(s[i], set) == 1 && len > 1)
+	{
+		i++;
+		len--;
+	}
+	while (s[j] && isset(s[j], set) == 1 && len > 1)
+	{
+		j--;
+		len--;
+	}
+	return (len);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
+	int		i;
+	int		j;
+	int		len;
+	int		trimlen;
+	char	*news;
 
+	if (!s1)
+		return (0);
+	i = 0;
+	j = 0;
+	len = ft_strlen(s1);
+	trimlen = ftrimlen(len, s1, set);
+	news = malloc((trimlen + 1) * sizeof(char));
+	if (!news)
+		return (0);
+	while (s1[i] && isset(s1[i], set) == 1)
+		i++;
+	while (j < trimlen)
+	{
+		news[j] = s1[i];
+		i++;
+		j++;
+	}
+	news[j] = '\0';
+	return (news);
+}
+
+int	main()
+{
+	printf("%s\n", ft_strtrim("abqbc", "abc"));
+	printf("%s\n", ft_strtrim("xavocadoyz", "xyz"));
 }
